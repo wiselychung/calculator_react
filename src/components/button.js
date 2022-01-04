eb767b885058bdd0ec9d6e164a7d63fb832f695c
Wisely <wiselychung96@gmail.com>
2022-01-04 11:06:26 +0800

    first commit

---
+++ src/components/button.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import App from '../App';
+
+
+class button extends Component{
+    render(){
+        return(
+            <div>
+                <button class = "calc-button">1</button>
+
+
+            </div>
+        );
+    }
+}
+
+export default button;
